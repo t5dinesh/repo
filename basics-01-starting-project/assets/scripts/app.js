53ea312f375b8b256d2ffcf9b6089a73594b87e2
dinesh <dinesh970416@gmail.com>
2021-01-25 17:47:16 +0530

    3

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,14 @@
+// var a = function("dinesh" , 23);
+// function myfunction(name,age){
+//     return name,age
+// }
+
+
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert("the result is " + result);
+}
+
+add(1, 2);
+add(5, 10)
